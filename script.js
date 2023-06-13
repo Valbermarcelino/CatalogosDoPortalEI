a84ae9f332880f901acd853dfade613435cb66ef
valber <valbermarcelino999@gmail.com>
2023-06-13 19:33:27 -0300

    Unify the request code

--- script.js
+++ script.js
@@ -30,48 +30,7 @@ const buttons = document.querySelectorAll('.button');
 	
 		let url = 'https://snowy-feather-log.glitch.me/conteudos';
 
-		fetch(url)
-		.then((response) => response.json())
-		.then(json => {
-			const data = json;
-			const catalogo = data;
-			
-			console.log(catalogo);
-			console.log(catalogo[0]);
-
-			catalogo.forEach(item => {		
-				var catalogElement = document.createElement('div');
-				catalogElement.classList.add('catalogElement');
-
-					var imageElement = document.createElement('div');
-					imageElement.classList.add('image');
-					imageElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><img src="${item.linkDaImagem}"></img></a>`;
-					catalogElement.appendChild(imageElement);	
-
-					
-					var textContent = document.createElement('div');
-					textContent.classList.add('textContent');
-						var titleElement = document.createElement('div');
-						titleElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><h3>${item.conteúdo}</h3></a>`;
-						textContent.appendChild(titleElement);
-
-						var descriptionElement = document.createElement('p');
-						descriptionElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><p>${item.descrição}</p></a>`;
-						textContent.appendChild(descriptionElement);
-
-						var tagsElement = document.createElement('p');
-						tagsElement.classList.add('tags');
-						
-						tagsElement.innerHTML += item.tags;
-                        
-						textContent.appendChild(tagsElement);
-					catalogElement.appendChild(textContent);
-
-				list.appendChild(catalogElement);
-
-				list.innerHTML += "<hr>";
-			});
-		});
+        getLabels(url);
 	}
 	
 	function fetchMetaversos(){
@@ -81,48 +40,7 @@ const buttons = document.querySelectorAll('.button');
 		
 		let url = 'https://snowy-feather-log.glitch.me/metaversos';
 		
-		fetch(url)
-		.then((response) => response.json())
-		.then(json => {
-			const data = json;
-			const catalogo = data;
-
-			catalogo.forEach((item, index) => {		
-				var catalogElement = document.createElement('div');
-				catalogElement.classList.add('catalogElement');
-				
-					function exibirImagem (linkDaImagem){	
-						var imageElement = document.createElement('div');
-						imageElement.classList.add('image');
-						imageElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><img src="${linkDaImagem}"></img></a>`;
-						catalogElement.appendChild(imageElement);	
-					}
-					exibirImagem(item.linkDaImagem);
-					
-					
-					var textContent = document.createElement('div');
-					textContent.classList.add('textContent');
-						var titleElement = document.createElement('div');
-						titleElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><h3>${item.ambiente}</h3></a>`;
-						textContent.appendChild(titleElement);
-
-						var descriptionElement = document.createElement('p');
-						descriptionElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><p>${item.descrição}</p></a>`;
-						textContent.appendChild(descriptionElement);
-
-						var tagsElement = document.createElement('p');
-						tagsElement.classList.add('tags');
-						
-						tagsElement.innerHTML += item.tags;
-
-						textContent.appendChild(tagsElement);
-					catalogElement.appendChild(textContent);
-
-				list.appendChild(catalogElement);
-
-				list.innerHTML += "<hr>";
-			});
-		});
+		getLabels(url);
 	}
 	
 	function fetchFerramentas(){
@@ -130,7 +48,19 @@ const buttons = document.querySelectorAll('.button');
 		
 		let url = 'https://snowy-feather-log.glitch.me/ferramentas';
 	
-		fetch(url)
+		getLabels(url);
+	}
+	
+	function fetchLaboratorios(){
+		list.innerHTML = '';
+	
+		let url = 'https://snowy-feather-log.glitch.me/laboratorios';
+	
+		getLabels(url);
+	}
+
+    function getLabels(url){
+        fetch(url)
 		.then((response) => response.json())
 		.then(json => {
 			const data = json;
@@ -154,7 +84,7 @@ const buttons = document.querySelectorAll('.button');
 						var lineContainer = document.createElement('div');
 						lineContainer.classList.add('lineContainer');
 							var titleElement = document.createElement('div');
-							titleElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><h3>${item.ferramenta}</h3></a>`;
+							titleElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><h3>${item.nome}</h3></a>`;
 							lineContainer.appendChild(titleElement);
 							
 							if (item.linkDoTutorial != ""){
@@ -181,55 +111,7 @@ const buttons = document.querySelectorAll('.button');
 				list.innerHTML += "<hr>";
 			});
 		});
-	}
-	
-	function fetchLaboratorios(){
-		list.innerHTML = '';
-	
-		let url = 'https://snowy-feather-log.glitch.me/laboratorios';
-	
-		fetch(url)
-		.then((response) => response.json())
-		.then(json => {
-			const data = json;
-			const catalogo = data;
-
-            console.log(catalogo);
-
-			catalogo.forEach(item => {		
-				var catalogElement = document.createElement('div');
-				catalogElement.classList.add('catalogElement');
-
-					var imageElement = document.createElement('div');
-					imageElement.classList.add('image');
-					imageElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><img src="${item.linkDaImagem}"></img></a>`;
-					catalogElement.appendChild(imageElement);	
-
-					
-					var textContent = document.createElement('div');
-					textContent.classList.add('textContent');
-						var titleElement = document.createElement('div');
-						titleElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><h3>${item.laboratório}</h3></a>`;
-						textContent.appendChild(titleElement);
-
-						var descriptionElement = document.createElement('p');
-						descriptionElement.innerHTML = `<a href="${item.linkDoConteúdo}" target="_blank"><p>${item.descrição}</p></a>`;
-						textContent.appendChild(descriptionElement);
-
-						var tagsElement = document.createElement('p');
-						tagsElement.classList.add('tags');
-						
-						tagsElement.innerHTML += item.tags;
-
-						textContent.appendChild(tagsElement);
-					catalogElement.appendChild(textContent);
-
-				list.appendChild(catalogElement);
-
-				list.innerHTML += "<hr>";
-			});
-		});
-	}
+    }
 	
 	// Simula o clique no botão 1 ao abrir a página 
     fetchConteudos();
